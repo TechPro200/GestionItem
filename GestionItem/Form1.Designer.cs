182e8e3a4bd3cd995cc23f791f61cc237701eefd
TechPro200 <tutankapopo@gmail.com>
2025-07-30 21:21:12 -0400

    v1

--- GestionItem/Form1.Designer.cs
+++ GestionItem/Form1.Designer.cs
@@ -28,12 +28,91 @@
         /// </summary>
         private void InitializeComponent()
         {
-            this.components = new System.ComponentModel.Container();
-            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
-            this.ClientSize = new System.Drawing.Size(800, 450);
-            this.Text = "Form1";
+            System.ComponentModel.ComponentResourceManager resources = new System.ComponentModel.ComponentResourceManager(typeof(Form1));
+            panel2 = new Panel();
+            RegistrarseBtn = new Button();
+            label4 = new Label();
+            ContraseñaText = new TextBox();
+            NombreText = new TextBox();
+            label3 = new Label();
+            label2 = new Label();
+            label1 = new Label();
+            panel2.SuspendLayout();
+            SuspendLayout();
+            // 
+            // panel2
+            // 
+            panel2.Controls.Add(RegistrarseBtn);
+            panel2.Controls.Add(label4);
+            panel2.Controls.Add(ContraseñaText);
+            panel2.Controls.Add(NombreText);
+            panel2.Controls.Add(label3);
+            panel2.Controls.Add(label2);
+            panel2.Controls.Add(label1);
+            resources.ApplyResources(panel2, "panel2");
+            panel2.Name = "panel2";
+            // 
+            // RegistrarseBtn
+            // 
+            resources.ApplyResources(RegistrarseBtn, "RegistrarseBtn");
+            RegistrarseBtn.Name = "RegistrarseBtn";
+            RegistrarseBtn.UseVisualStyleBackColor = true;
+            // 
+            // label4
+            // 
+            resources.ApplyResources(label4, "label4");
+            label4.Name = "label4";
+            // 
+            // ContraseñaText
+            // 
+            resources.ApplyResources(ContraseñaText, "ContraseñaText");
+            ContraseñaText.Name = "ContraseñaText";
+            // 
+            // NombreText
+            // 
+            resources.ApplyResources(NombreText, "NombreText");
+            NombreText.Name = "NombreText";
+            // 
+            // label3
+            // 
+            resources.ApplyResources(label3, "label3");
+            label3.Name = "label3";
+            // 
+            // label2
+            // 
+            resources.ApplyResources(label2, "label2");
+            label2.Name = "label2";
+            // 
+            // label1
+            // 
+            resources.ApplyResources(label1, "label1");
+            label1.Name = "label1";
+            // 
+            // Form1
+            // 
+            resources.ApplyResources(this, "$this");
+            AutoScaleMode = AutoScaleMode.Font;
+            BackColor = SystemColors.ButtonFace;
+            BackgroundImage = Properties.Resources.guia_almacen_7;
+            Controls.Add(panel2);
+            ForeColor = SystemColors.ControlText;
+            Name = "Form1";
+            SizeGripStyle = SizeGripStyle.Show;
+            TransparencyKey = Color.FromArgb(255, 192, 128);
+            Load += Form1_Load;
+            panel2.ResumeLayout(false);
+            panel2.PerformLayout();
+            ResumeLayout(false);
         }
 
         #endregion
+        private Panel panel2;
+        private Label label1;
+        private Label label3;
+        private Label label2;
+        private Button RegistrarseBtn;
+        private Label label4;
+        private TextBox ContraseñaText;
+        private TextBox NombreText;
     }
 }
--- GestionItem/Form1.cs
+++ GestionItem/Form1.cs
@@ -6,5 +6,15 @@ namespace GestionItem
         {
             InitializeComponent();
         }
+
+        private void Form1_Load(object sender, EventArgs e)
+        {
+
+        }
+
+        private void panel1_Paint(object sender, PaintEventArgs e)
+        {
+
+        }
     }
 }
